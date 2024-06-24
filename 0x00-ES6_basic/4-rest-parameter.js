f5b86687afae06f1042dd46b12565130048783b5
malhaouit <abdelmounaim.abdel@gmail.com>
2024-06-24 22:48:00 +0000

    Update: Reduce the code

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,10 +1,3 @@
 export default function returnHowManyArguments(...args) {
-  let count = 0;
-
-  // eslint-disable-next-line no-unused-vars
-  for (const arg of args) {
-    count += 1;
-  }
-
-  return count;
+  return args.length;
 }
