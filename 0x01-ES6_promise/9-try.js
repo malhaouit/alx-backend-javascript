9b4a0b11e9f399e55dbfea6ba1762adb68bb6910
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-01 23:29:57 +0000

    Update: Move the additin of message `Guardrail was processed` outside `try` `catch`

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+
   try {
-    queue.push(mathFunction(), 'Guardrail was processed');
+    queue.push(mathFunction());
   } catch (error) {
-    queue.push(`Error: ${error.message}`, 'Guardrail was processed');
+    queue.push(`Error: ${error.message}`);
   }
+  queue.push('Guardrail was processed');
 
   return queue;
 }
