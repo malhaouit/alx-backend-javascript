6e73d3f80baa8d48ec1a09006d4fadfdaaa485c8
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-05 23:20:58 +0000

    Write a String literal type named Subjects

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,12 @@ function executeWork(employee: Director | Teacher): void {
   }
 }
 
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History';
+  }
+}
