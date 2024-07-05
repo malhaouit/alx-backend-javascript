848d3baeaf5390d73867eeef741bc5d072b1d329
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-05 23:55:17 +0000

    Update main.ts to create and export subject instances and teacher, set teacher, and log method outputs

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,28 @@
+import { Subjects } from "./subjects/Cpp";
+import { Subjects as JavaSubjects } from "./subjects/Java";
+import { Subjects as ReactSubjects } from "./subjects/React";
+
+export const cpp = new Subjects.Cpp();
+export const java = new JavaSubjects.Java();
+export const react = new ReactSubjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  experienceTeachingC: 10
+};
+
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
