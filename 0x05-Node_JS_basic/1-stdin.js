b5b07c8235a14e616788cba6055835892fdea45a
malhaouit <abdelmounaim.abdel@gamil.com>
2024-08-19 15:47:52 +0000

    Update the code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
+  const chunk = process.stdin.read();
 
-  if (name !== null) {
-    console.log(`Your name is: ${name.trim()}`);
-    process.stdin.end();
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
