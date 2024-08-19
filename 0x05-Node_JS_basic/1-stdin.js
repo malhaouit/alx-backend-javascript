3cb404c567bc370882b0cac4490aae327e6a407e
malhaouit <abdelmounaim.abdel@gamil.com>
2024-08-19 15:40:58 +0000

    Update: add null check

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,11 +3,12 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-  const name = process.stdin.read().trim();
+  const name = process.stdin.read();
 
-  console.log(`Your name is: ${name}`);
-
-  process.stdin.end();
+  if (name !== null) {
+    console.log(`Your name is: ${name.trim()}`);
+    process.stdin.end();
+  }
 });
 
 process.stdin.on('end', () => {
