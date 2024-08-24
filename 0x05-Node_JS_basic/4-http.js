634e41237ac4bcb5be1f70789fcb3257cacbd32a
malhaouit <abdelmounaim.abdel@gamil.com>
2024-08-24 14:44:41 +0000

    Update: export the module and add the localhost

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,6 +4,8 @@ const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server running at <http://localhost:1245/>');
+app.listen(1245, 'localhost', () => {
+  console.log('Server running at http://localhost:1245/');
 });
+
+module.exports = app;
