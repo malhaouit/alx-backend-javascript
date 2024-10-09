acd4a00910e8826e1a21ecd7553dfdd29968eae2
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-09 14:27:38 +0000

    Implement sendPaymentRequestToApi with spy tests and refactor Utils module

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber with SUM and the right arguments', function () {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+
+    spy.restore();  // Always restore the spy after use
+  });
+});
